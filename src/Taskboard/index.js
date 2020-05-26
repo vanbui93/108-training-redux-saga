5b4f7e37efd4d2f9692188241761c05ef200e2cb
Van Bui <van.bt@trans-cosmos.com.vn>
2020-05-26 14:00:57 +0700

    [Material] STYLE Component 2

--- src/Taskboard/index.js
+++ src/Taskboard/index.js
@@ -6,17 +6,48 @@ import { Add } from '@material-ui/icons';
 import Grid from '@material-ui/core/Grid';
 import { STATUSES } from './../constants/index';
 
-class TaskBoard extends Component {
+const listTask = [
+  {
+    id: 1,
+    title: "ReadBook",
+    description: "Read material ui book",
+    status: 0
+  },
+  {
+    id: 2,
+    title: "Play Football",
+    description: "With my friend",
+    status: 2
+  },
+  {
+    id: 3,
+    title: "Play game",
+    description: "",
+    status: 3
+  }
+];
 
+class TaskBoard extends Component {
   renderBoard = () => {
+    const { classes } = this.props;
     let xhtml = null;
     xhtml = (
       <Grid container spacing={3}>
         {
           STATUSES.map((status, index) => {
+            const taskFilter = listTask.filter(taskFil => taskFil.status === status.value)
             return (
               <Grid item md={4} xs={12} key={index}>
-                {status.label}
+                <div className={classes.status}>{status.label}</div>
+                <div className={classes.wraperListTask}>
+                  {
+                    taskFilter.map((task) => {
+                      return (
+                        <h1>{task.title}</h1>
+                      )
+                    })
+                  }
+                </div>
               </Grid>
             )
           })
