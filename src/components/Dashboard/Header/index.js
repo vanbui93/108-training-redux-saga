dd08d2b57f5461ec7d76aa6ffb033cf9441c77d4
Van Bui <van.bt@trans-cosmos.com.vn>
2020-06-18 14:15:12 +0700

    152 [MATERIAL-UI] Xay dung Header -> Edit Layout

--- src/components/Dashboard/Header/index.js
+++ src/components/Dashboard/Header/index.js
@@ -1,19 +1,121 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/styles';
-import styles from './styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Badge from '@material-ui/core/Badge';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import SearchIcon from '@material-ui/icons/Search';
+import { withStyles } from '@material-ui/styles';
+import React, { Component } from 'react';
+import styles from './styles';
+
+const menuId = 'primary-search-account-menu';
+const mobileMenuId = 'primary-search-account-menu-mobile';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileMoreAnchorEl: null,
+      anchorEl: null,
+      setMobileMoreAnchorEl: null,
+      isMobileMenuOpen: false,
+      isMenuOpen: false,
+
+    }
+  }
+
+  handleProfileMenuOpen = () => {
+    console.log('handleProfileMenuOpen');
+  }
+  handleMobileMenuOpen = () => {
+    console.log('handleProfileMenuOpen');
+  }
+
+  handleMobileMenuClose = () => {
+    console.log('handleMobileMenuClose');
+  }
+
+  handleProfileMenuOpen = () => {
+    console.log('handleProfileMenuOpen');
+  }
+
+  handleMenuClose = () => {
+    console.log('handleMenuClose');
+  }
+
+  renderMenu = () => {
+    const { anchorEl,isMenuOpen } = this.state;
+    return (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
+      </Menu>
+    )
+  };
+
+  renderMobileMenu = () => {
+    const { mobileMoreAnchorEl, isMobileMenuOpen } = this.state;
+    return (
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={this.handleMobileMenuClose}
+      >
+        <MenuItem>
+          <IconButton aria-label="show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <p>Messages</p>
+        </MenuItem>
+        <MenuItem>
+          <IconButton aria-label="show 11 new notifications" color="inherit">
+            <Badge badgeContent={11} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <p>Notifications</p>
+        </MenuItem>
+        <MenuItem onClick={this.handleProfileMenuOpen}>
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <p>Profile</p>
+        </MenuItem>
+      </Menu>
+    )
+  };
+
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.root}>
+      <div className={classes.grow}>
         <AppBar position="static">
           <Toolbar>
             <IconButton
@@ -40,12 +142,48 @@ class Header extends Component {
                 inputProps={{ 'aria-label': 'search' }}
               />
             </div>
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+              <IconButton aria-label="show 4 new mails" color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                  <MailIcon />
+                </Badge>
+              </IconButton>
+              <IconButton aria-label="show 17 new notifications" color="inherit">
+                <Badge badgeContent={17} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={this.handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={this.handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </div>
           </Toolbar>
         </AppBar>
+        {this.renderMobileMenu()}
+        {this.renderMenu()}
       </div>
     );
-  }
-}
+  };
+};
 
 
 export default withStyles(styles)(Header);
