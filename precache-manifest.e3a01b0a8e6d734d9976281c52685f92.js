cdb9df1f69bdd921920f323095a43dc8ce0e6df7
Van Bui <van.bt@trans-cosmos.com.vn>
2020-06-22 11:50:21 +0700

    Updates

---
+++ precache-manifest.e3a01b0a8e6d734d9976281c52685f92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22935a319a4cd1f469636d7f5db05405",
+    "url": "/108-training-redux-saga/index.html"
+  },
+  {
+    "revision": "ed2dc72b8b82eeb210f3",
+    "url": "/108-training-redux-saga/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ed2dc72b8b82eeb210f3",
+    "url": "/108-training-redux-saga/static/js/2.aa20f400.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/108-training-redux-saga/static/js/2.aa20f400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff8dce82845e31ea8a8",
+    "url": "/108-training-redux-saga/static/js/main.a58c0b33.chunk.js"
+  },
+  {
+    "revision": "f5331bcb1d4f0bd06ed7",
+    "url": "/108-training-redux-saga/static/js/runtime-main.239741e4.js"
+  },
+  {
+    "revision": "73abf0126abe075d13d9bc7873690a36",
+    "url": "/108-training-redux-saga/static/media/loading.73abf012.gif"
+  }
+]);
